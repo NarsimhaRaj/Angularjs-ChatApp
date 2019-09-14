d56aba823168f5d0db069aabc455849e3060842f
MobiComp <narsimharaj.kasu01@gmail.com>
2019-09-14 11:44:34 +0530

    comments added

--- Server/Middleware/token.js
+++ Server/Middleware/token.js
@@ -1,11 +1,17 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-
+/**
+ * @description: verifies a token and sends true or false 
+ * @param {token}, a token 
+ */
 exports.verifyToken = (token) => {
     return jwt.verify(token, process.env.KEY);
 }
 
-
+/**
+ * @description : generates a token for given data 
+ * @param {data}, data is a object, has user email and id 
+ */
 exports.generateToken = (data) => {
     return jwt.sign(data, process.env.KEY, { expiresIn: "1h" });
 }
