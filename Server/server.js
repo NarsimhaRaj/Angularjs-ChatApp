c74da794b00fb8582b1f518144f5c98a0ab6ddcc
MobiComp <narsimharaj.kasu01@gmail.com>
2019-09-18 09:55:53 +0530

    CORS added

--- Server/server.js
+++ Server/server.js
@@ -35,9 +35,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(expressValidator())
-//registeration route
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use('/chat_app', Router);
-const port = 3000
+const port = 5064
 app.listen(port,()=>{
     console.log("listening on port "+port);
 });
