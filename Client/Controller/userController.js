489b552e3f4fccf6ca528406cd47fb6495de1796
MobiComp <narsimharaj.kasu01@gmail.com>
2019-09-23 16:37:41 +0530

    added fetchConversation httpservice,refreshing fetch everytime listening receiving event

--- Client/Controller/userController.js
+++ Client/Controller/userController.js
@@ -3,30 +3,37 @@
 
     app.controller("mainController", Main);
 
-    var socket=io.connect("http://localhost:5064");
+    var socket = io.connect("http://localhost:5064");
 
     app.value("sender", "");
 
     var sender = null;
-
-    function Main($location, httpService) {
+    /**
+     * @description : Main controller has functions as login,forgot
+     * @param {$scope} , Controller Scope varibale
+     * @param {$location}  , location used to redirect to given path
+     * @param {httpService}  , calls $http services on being called
+     */
+    function Main($scope, $location, httpService) {
         var self = this;
-        this.editMode = true;
         this.successMode = undefined;
         this.errorMode = undefined;
-        this.username = "";
-        this.password = "";
-        this.textArea = "";
-        this.username = "";
         this.flag = false;
         httpService.getService()
             .then((response) => {
                 if (response.status)
-                    self.details = response.data;
+                    self.details = response.data.filter((friendsId) => {
+                        return sender != friendsId.email;
+                    });
                 else
                     self.details = response.error;
             });
 
+        /**
+         * @description: this singin function takes email and password as arguments and  calls sigin in function in httpServices 
+         * @param {email}, email of registered user  
+         * @param {password}, registered user password
+         */
         this.singin = function (email, password) {
             sender = email;
             this.user = { email: email, password: password }
@@ -43,6 +50,10 @@
             self.username = ""
             self.password = ""
         }
+        /**
+         * @description : creates a User object with details mensioned sends data to sever using $http services
+         * if data valid response comes registerd successfully if not error comes 
+         */
         this.register = function (username, firstname, lastname, email, password, confirmPassword) {
             var user = {
                 username: username,
@@ -56,12 +67,9 @@
                 .then(function (response) {
                     if (response.status) {
                         self.successMode = response.message;
-                        self.username = ""
-                        self.firstname = ""
-                        self.lastname = ""
-                        self.email = ""
-                        self.password = ""
-                        self.confirmPassword = ""
+                        self.username = ""; self.firstname = ""
+                        self.lastname = ""; self.email = ""
+                        self.password = ""; self.confirmPassword = ""
                         alert(response.message);
                         $location.path("/login");
                     }
@@ -70,10 +78,11 @@
                         self.errorMode = response.errors;
                     }
                 });
-            this.username = ""
-            this.password = ""
-            //  this.editMode = true;
         }
+        /**
+         * @description : takes email as an argument and calls forgotPassword function in UserServices 
+         * if response status is true from server mail has been sent to given email if user is a client
+         */
         this.forgotPassword = function (email) {
             httpService.forgotPassword({ email: email }).then((response) => {
                 if (response.status)
@@ -82,34 +91,84 @@
                     self.errorMode = response.errors;
             })
         }
+
+        /**
+         * @description : takes 2 arguments from input text given by user, passes them with token param to reset 
+         * password http service function
+         * @param {password}, password input element value;
+         * @param {confirmPassword}, confirm password input element value 
+         */
+        this.resetPassword = function (password, confirmPassword) {
+            httpService.resetPassword(token, { password: password, confirmPassword: confirmPassword })
+                .then((response) => {
+                    if (response.status) {
+                        this.successMode = response.message;
+                        alert(response.message)
+                    }
+                    else {
+                        this.errorMode = response.error;
+                        alert(response.error);
+                    }
+                })
+        }
+
+        /**
+         * @description : friends chat History will be shown on clicking on friends in contact details
+         * @param {receiver}, particular receiver email to whom sender wants to sent
+         */
         this.chatHistory = (receiver) => {
             this.flag = true;
             this.receiver = receiver;
-            httpService.fetchConversation({ sender: sender, receiver: this.receiver}).then((response)=>{
-                if(response.status)
-                    this.messages=response.message.messages;
-                else
-                    this.messages=response.error;
-            });
-            socket.on('receiving',function(responseMessage){
-                this.messages.push(responseMessage);
-               console.log(this.messages); 
-            })
+            $scope.fetch();
         }
-        
+
+        /**
+         * @description:on clicking send button on client side and emits an event at server side, sends chat data object containing 
+         * sender, receiver and message  
+         * @param {message}, message from sender
+         */
         this.send = (message) => {
-            var chatData = { sender: sender, receiver: this.receiver, message: message }
-            socket.emit('sending',chatData);
+            if (message != "" || message != null || message != undefined) {
+                var chatData = { sender: sender, receiver: this.receiver, message: message }
+                socket.emit('sending', chatData);
+            }
+            else {
+                alert("Please enter some text ");
+            }
         }
-        
+
+        socket.on('receiving', function (responseMessage) {
+            $scope.fetch();
+        })
+
+        /**
+         * @description : fetch chat Conversation between sender and receiver
+         */
+        $scope.fetch = () => {
+            console.log(sender + " " + this.receiver);
+            httpService.fetchConversation({ sender: sender, receiver: this.receiver })
+                .then((response) => {
+                    if (response.status)
+                        this.messagesPacket = response.message.conversations;
+                    else
+                        this.messagesPacket = response.error;
+                });
+        }
+
+
 
         //redirect to register page on clicking register button
         this.redirectToRegister = function () {
             $location.path("/register");
         }
-        //redirects to forgotpassword page on clicking forgotPassword button
-        this.redirectToForgotPassword = function () {
-            $location.path('/forgotPassword');
+
+        /**
+         * @description : align messages right side if messages are from sender 
+         * @param {userId}, userId is message sent person's id
+         */
+        this.alignMessageRight = (userId) => {
+            return userId == sender ? true : false;
         }
+
     }
 })();
--- Client/Services/userServices.js
+++ Client/Services/userServices.js
@@ -1,8 +1,13 @@
 (function () {
     var app = angular.module("chatApp");
 
-
-    app.service("httpService",function ($http,$location) {
+    /**
+     * @description : calls constructor with $http services, whose url's are server side routes 
+    */
+    app.service("httpService",function ($http) {
+        /**
+         * @description : uses http get method, which gets all the registered users if no error resolves, otherwise rejects(error response)
+         */
           this.getService = () => {
             return $http.get("http://localhost:5064/chat_app/getUsers").then(function (response) {
                 return response.data;
@@ -11,6 +16,10 @@
                 return error.data;
             });
         }
+        /**
+         * @description : uses http post service, verifies user at server side and gets response if no error resolves, otherwise rejects(error response)
+         * @param {user}, user is an object contains email,password
+         */
           this.postLoginService = (user) => {
             return $http.post("http://localhost:5064/chat_app/login", user).then(function (response) {
                 return response.data;
@@ -19,6 +28,11 @@
             });
 
         }
+        /**
+         * @description : uses http post service, store all the details in mongo if no error in details, if not, receives error response with status
+         * codes that will be rejected 
+         * @param {user}, object with details like username, firstname, lastname, email, password, confirmpassword
+         */
           this.register=(user)=>{
             return $http.post("http://localhost:5064/chat_app/register", user).then(function (response) {
                 return response.data;
@@ -26,6 +40,11 @@
                 return error.data;
             });
         }
+        /**
+         * @description : uses http post service, sends email in request body and access it server side, if registered, an email sent for resetting 
+         * password
+         * @param {email}, registered user email
+         */
         this.forgotPassword=(email)=>{
             return $http.post("http://localhost:5064/chat_app/forgotPassword",email).then(function(response){
                 return response.data
@@ -33,13 +52,39 @@
             return error.data;
         })
         }
-
+        /**
+         * @description : uses http post service, To fetch whole conversation between sender and receiver, if no conversion sends an error 
+         * response saying no messages to show 
+         * @chatData : it's a object containing sender, receiver details 
+         */
         this.fetchConversation=(chatData)=>{
             return $http.post("http://localhost:5064/chat_app/fetchConversation",chatData).then(function(response){
                 return response.data
         },function(error){
+            console.log(error.data)
             return error.data;
         })
         }
+        /**
+         * @description : uses http post service, sends token as route parameter and password, confirm Password as request body
+         * @param {token}, token is jwt token taken from route params
+         * @param {passwordChangeData}, it's a object contains password and confirmpassword as properties
+         */
+        this.resetPassword=(token,passwordChangeData)=>{
+            return $http({
+                method: 'post',
+                headers : {
+                    "Content-Type":"application/json",
+                    "authentication":token
+                },
+                url: "http://localhost:5064/chat_app/resetPassword",
+                data : passwordChangeData
+            }).then(function(response){
+                return response.data;
+            },function(error){
+                return error.data;
+            })
+        }
+
     });
 })();
